1bc76c9d206b976049798acf2232ff9b77be78c5
Felix Adrian <sionirvine@gmail.com>
2013-09-26 00:33:46 +0700

    Revamp Camera Handling
    Completely change camera handling code so it works better,
    brings another improvements:
    - app pausing & resuming crash fixed
    - camera preview size is optimal
    - app loading times improved significantly
    - changed layout to directly use fragments

--- src/app/istts/ar/CameraFragment.java
+++ src/app/istts/ar/CameraFragment.java
@@ -4,55 +4,58 @@ package app.istts.ar;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
-
-import java.util.List;
+import android.widget.Toast;
 
 public class CameraFragment extends Fragment {
-
+    
+    private final static String TAG = "iSTTSAR::CameraFragment";
+    
     private Camera mCamera;
     private CameraPreview mPreview;
 
-    private static final int PICTURE_SIZE_MAX_WIDTH = 1280;
-    private static final int PREVIEW_SIZE_MAX_WIDTH = 640;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (checkCameraHardware(this.getActivity())) {
+            mPreview = new CameraPreview(this.getActivity());
+        } else {
+            Toast.makeText(getActivity(), "Camera is not found!", Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        RelativeLayout mLayout = (RelativeLayout) inflater.inflate(
-                R.layout.activity_camera_fragment, container, false);
-
-        if (checkCameraHardware(getActivity().getApplicationContext())) {
-            // Create an instance of Camera
-            mCamera = getCameraInstance();
-
-            // Create our Preview view and set it as the content of our
-            // activity.
-            mPreview = new CameraPreview(getActivity().getApplicationContext(), mCamera);
-            FrameLayout preview = (FrameLayout) mLayout.findViewById(R.id.camera_preview);
-            preview.addView(mPreview);
-        }
-
-        return mLayout;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return mPreview;
     }
 
     @Override
     public void onPause() {
         super.onPause();
-
-        releaseCamera(); // release the camera immediately on pause event
+        
+        if (mCamera != null) {
+            mPreview.setCamera(null);
+            releaseCamera(); // release the camera immediately on pause event
+        }
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        
+        mCamera = getCameraInstance();
+        mPreview.setCamera(mCamera);
     }
 
     /** CAMERA **/
 
-    /** Check if device has a camera */
+    /** Check if device has a camera **/
     private boolean checkCameraHardware(Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
             return true;
@@ -61,68 +64,25 @@ public class CameraFragment extends Fragment {
         }
     }
 
-    /** A safe way to get an instance of the Camera object. */
+    /** A safe way to get an instance of the Camera object. **/
     public static Camera getCameraInstance() {
         Camera c = null;
         try {
             c = Camera.open();
         } catch (Exception e) {
             // Camera is not available
+            Log.d(TAG, "Camera is not available!");
         }
         return c;
     }
 
+    /** release the camera for other applications **/
     private void releaseCamera() {
         if (mCamera != null) {
-            mCamera.release(); // release the camera for other applications
+            mCamera.stopPreview();
+            mCamera.release();
             mCamera = null;
         }
-    }
-
-    public void setupCamera() {
-        Camera.Parameters parameters = mCamera.getParameters();
-
-        Size bestPreviewSize = determineBestPreviewSize(parameters);
-        Size bestPictureSize = determineBestPictureSize(parameters);
-
-        parameters.setPreviewSize(bestPreviewSize.width, bestPreviewSize.height);
-        parameters.setPictureSize(bestPictureSize.width, bestPictureSize.height);
-
-        mCamera.setParameters(parameters);
-    }
-
-    private Size determineBestPreviewSize(Camera.Parameters parameters) {
-        List<Size> sizes = parameters.getSupportedPreviewSizes();
-
-        return determineBestSize(sizes, PREVIEW_SIZE_MAX_WIDTH);
-    }
-
-    private Size determineBestPictureSize(Camera.Parameters parameters) {
-        List<Size> sizes = parameters.getSupportedPictureSizes();
-
-        return determineBestSize(sizes, PICTURE_SIZE_MAX_WIDTH);
-    }
-
-    protected Size determineBestSize(List<Size> sizes, int widthThreshold) {
-        Size bestSize = null;
-
-        for (Size currentSize : sizes) {
-            boolean isDesiredRatio = (currentSize.width / 4) == (currentSize.height / 3);
-            boolean isBetterSize = (bestSize == null || currentSize.width > bestSize.width);
-            boolean isInBounds = currentSize.width <= PICTURE_SIZE_MAX_WIDTH;
-
-            if (isDesiredRatio && isInBounds && isBetterSize) {
-                bestSize = currentSize;
-            }
-        }
-
-        if (bestSize == null) {
-            // listener.onCameraError();
-
-            return sizes.get(0);
-        }
-
-        return bestSize;
     }
 
 }
--- src/app/istts/ar/CameraPreview.java
+++ src/app/istts/ar/CameraPreview.java
@@ -2,63 +2,78 @@ package app.istts.ar;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.io.IOException;
+import java.util.List;
 
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+public class CameraPreview extends ViewGroup implements SurfaceHolder.Callback {
 
     private final static String TAG = "iSTTSAR::CameraPreview";
-    private SurfaceHolder mHolder;
+
     private Camera mCamera;
-    
-    private static final double ASPECT_RATIO = 4.0 / 3.0;
+
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mHolder;
+    private Boolean mSurfaceCreated = false;
+
+    private Size mPreviewSize;
+    private List<Size> mSupportedPreviewSizes;
+
+    // private static final double ASPECT_RATIO = 4.0 / 3.0;
     
     @SuppressWarnings("deprecation")
-    public CameraPreview(Context context, Camera camera) {
+    public CameraPreview(Context context) {
         super(context);
         
-        mCamera = camera;
-
+        mSurfaceView = new SurfaceView(context);
+        addView(mSurfaceView);
+        
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
-        mHolder = getHolder();
-        mHolder.addCallback(this);
+        mHolder = mSurfaceView.getHolder();
         // deprecated setting, but required on Android versions prior to 3.0
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mHolder.addCallback(this);
     }
     
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
-        try {
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        if (mCamera != null) {
+            try {
+                mCamera.setPreviewDisplay(holder);
+            } catch (IOException e) {
+                Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+            }
         }
+        if (mPreviewSize == null)
+            requestLayout();
+        mSurfaceCreated = true;
     }
     
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-            int height) {
-        
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
-
+        
         if (mHolder.getSurface() == null){
-          // preview surface does not exist
-          return;
+            // preview surface does not exist
+            return;
         }
 
         // stop preview before making changes
         try {
             mCamera.stopPreview();
         } catch (Exception e){
-          // ignore: tried to stop a non-existent preview
+            // ignore: tried to stop a non-existent preview
         }
 
         // set preview size and make any resize, rotate or
@@ -66,9 +81,12 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
         // start preview with new settings
         try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+            requestLayout();
 
+            mCamera.setParameters(parameters);
+            mCamera.startPreview();
         } catch (Exception e){
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
         }
@@ -76,20 +94,113 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        // empty. Take care of releasing the Camera preview in your activity.
+        if (mCamera != null)
+            mCamera.stopPreview();
+    }
+    
+    public void setCamera(Camera camera) {
+        mCamera = camera;
+        if (mCamera != null) {
+            mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+
+            if (mSurfaceCreated)
+                requestLayout();
+        }
     }
     
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int height = MeasureSpec.getSize(heightMeasureSpec);
-        int width = MeasureSpec.getSize(widthMeasureSpec);
 
-        if (width > height * ASPECT_RATIO) {
-             width = (int) (height * ASPECT_RATIO + .5);
-        } else {
-            height = (int) (width / ASPECT_RATIO + .5);
-        }
+        // SET 4:3 RATIO
+        // int height = MeasureSpec.getSize(heightMeasureSpec);
+        // int width = MeasureSpec.getSize(widthMeasureSpec);
+        //
+        // if (width > height * ASPECT_RATIO) {
+        // width = (int) (height * ASPECT_RATIO + .5);
+        // } else {
+        // height = (int) (width / ASPECT_RATIO + .5);
+        // }
+
+        final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
 
         setMeasuredDimension(width, height);
+        setMeasuredDimension(width, height);
+
+        if (mSupportedPreviewSizes != null)
+            mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
+
+        if (mCamera != null) {
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+
+            mCamera.setParameters(parameters);
+        }
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (getChildCount() > 0) {
+            final View child = getChildAt(0);
+
+            final int width = r - l;
+            final int height = b - t;
+
+            int previewWidth = width;
+            int previewHeight = height;
+
+            if (mPreviewSize != null) {
+                previewWidth = mPreviewSize.width;
+                previewHeight = mPreviewSize.height;
+            }
+
+            // Center the child SurfaceView within the parent.
+            if (width * previewHeight > height * previewWidth) {
+                final int scaledChildWidth = previewWidth * height / previewHeight;
+                child.layout((width - scaledChildWidth) / 2, 0,
+                        (width + scaledChildWidth) / 2, height);
+            } else {
+                final int scaledChildHeight = previewHeight * width / previewWidth;
+                child.layout(0, (height - scaledChildHeight) / 2, width,
+                        (height + scaledChildHeight) / 2);
+            }
+        }
+
+    }
+
+    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) w / h;
+        if (sizes == null)
+            return null;
+
+        Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Try to find an size match aspect ratio and size
+        for (Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
+                continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find the one match the aspect ratio, ignore the requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
     }
 }
